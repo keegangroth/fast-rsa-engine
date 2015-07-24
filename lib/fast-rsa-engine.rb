d26b0d5568f9ce77efa9bdea78bc615e8fb784e1
R. Tyler Croy <tyler@monkeypox.org>
2015-07-24 13:41:45 -0700

    Add support for including fast-rsa-engine as a no-op dependency on MRI
    This will allow users of this gem to include it as a dependency, even if all of
    their use-cases aren't on the JVM. Obviating the need for if statements all
    over the place just to get faster RSA ops

--- lib/fast-rsa-engine.rb
+++ lib/fast-rsa-engine.rb
@@ -1,13 +1,17 @@
-require 'fast-rsa-engine_jars.rb'
-require 'fast-rsa-engine.jar'
-require 'openssl'
+if RUBY_PLATFORM == 'java'
+  require 'fast-rsa-engine_jars.rb'
+  require 'fast-rsa-engine.jar'
+  require 'openssl'
 
-# keep the default name space clean and use tap
-tap do
-  engines = Java::OrgJrubyExtOpenssl::SecurityHelper.java_class.declared_field 'implEngines'
-  engines.accessible = true
-  com.github.lookout.fastrsa.SecurityHelperMap.setup( engines.value( Java::OrgJrubyExtOpenssl::SecurityHelper ))
-  use_internal = Java::OrgJrubyExtOpenssl::SecurityHelper.java_class.declared_field 'tryCipherInternal'
-  use_internal.accessible = true
-  use_internal.set_value( Java::OrgJrubyExtOpenssl::SecurityHelper, true )
+  # keep the default name space clean and use tap
+  tap do
+    engines = Java::OrgJrubyExtOpenssl::SecurityHelper.java_class.declared_field 'implEngines'
+    engines.accessible = true
+    com.github.lookout.fastrsa.SecurityHelperMap.setup( engines.value( Java::OrgJrubyExtOpenssl::SecurityHelper ))
+    use_internal = Java::OrgJrubyExtOpenssl::SecurityHelper.java_class.declared_field 'tryCipherInternal'
+    use_internal.accessible = true
+    use_internal.set_value( Java::OrgJrubyExtOpenssl::SecurityHelper, true )
+  end
+else
+  $stderr.write("fast-rsa-engine does not affect MRI")
 end
